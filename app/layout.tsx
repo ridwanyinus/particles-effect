35e42c0d298582537d7856e8d861e2d005dc3b05
Ridwan <ridwanyinusa15@gmail.com>
2024-08-06 19:18:11 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
